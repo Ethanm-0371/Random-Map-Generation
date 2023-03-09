80b5e6206d862e4357e4bb8df3d79ef66e8504aa
Ethanm-0371 <ethanm0371@gmail.com>
2023-03-09 02:54:07 +0100

    Added all of the solution and  main program files.

---
+++ Game/Source/RandomGenerator.h
@@ -0,0 +1,17 @@
+#ifndef __RANDOMGENERATOR_H__
+#define __RANDOMGENERATOR_H__
+
+#include "App.h"
+#include "Module.h"
+#include "Point.h"
+
+struct Triangle
+{
+
+};
+
+class RandomGenerator
+{
+};
+
+#endif // __RANDOMGENERATOR_H__
